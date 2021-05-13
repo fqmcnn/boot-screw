635bf638467bdd6f8363829101db7e0d5bf9d1b5
T25679 <fengqiming001@deppon.com>
2021-05-13 23:29:42 +0800

    boot2.0集成screw

---
+++ src/main/java/com/panion/space/BootScrewApplication.java
@@ -0,0 +1,13 @@
+package com.panion.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootScrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootScrewApplication.class, args);
+    }
+
+}
